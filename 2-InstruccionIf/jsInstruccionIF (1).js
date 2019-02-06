9de87018e555bb7fb7b04b98b4d51b5a1ffb1249
KATAL3PSY <47353542+KATAL3PSY@users.noreply.github.com>
2019-02-05 21:28:03 -0300

    1-2

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,24 @@ function mostrar()
 {
 //tomo la edad  
 
+var x;
+x=document.getElementById('edad').value;
+	
+
+
+
+
+
+	if(x=15);
+		alert("niña bonita");	
+	
+	else;
+		alert("");
+
+
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,14 @@ function mostrar()
 {
 //tomo la edad  
 
+var x;
+x=getElementById('edad').value;
+
+
+if(x>=18);
+	alert("es mayor de edad")
+
+
 
 
 }//FIN DE LA FUNCIÓN
