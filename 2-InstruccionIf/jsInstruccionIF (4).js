223ccda977a8db546cdd1b551f517d632ae4748f
unknown <maati0133@gmail.com>
2019-02-08 19:42:11 -0300

    if

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -3,20 +3,37 @@ function mostrar()
 //tomo la edad  
 
 
-var edad;
-edad=document.getElementById('edad').value;
+var edad1;
+edad1=document.getElementById('edad').value;
 
 
-if(17>edad)
-	alert("es adolescente");
-//if(13<edad)
-	//alert("es adolescente");
-
-else if
-	(13<edad);
 
 
-else;
+if(edad1<17)
+	alert("usted es adolescente");
+
+
+	else if(edad1>13)
+
+		alert("usted es adolescente")
+
+else
+	alert("es menor de edad");
+
+// switch(edad.value) {
+//   case 13:
+//     alert("usted es un adolescente");
+//     break;
+//   case 15:
+//     alert("usted es un adolescente");
+//     break;
+//   case 16:
+//     alert("usted es un adolescente");
+//     break;
+//   case 17:
+//     alert("usted es un adolescente");
+// }
+
 
 
 
@@ -30,4 +47,4 @@ else;
 
 }
 
-/*FIN DE LA FUNCIÓN
+/*FIN DE LA FUNCIÓN*/
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -3,4 +3,24 @@ function mostrar()
 //tomo la edad  
 
 
+var edad=document.getElementById('edad').value
+
+if(edad)		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }//FIN DE LA FUNCIÓN
