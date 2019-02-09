29e895c0ad2f2f1755abdc5c936c476c6c2e9f27
unknown <maati0133@gmail.com>
2019-02-08 23:09:07 -0300

    ej 10 if

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -3,4 +3,28 @@ function mostrar()
 	//Genero el número RANDOM entre 1 y 10 
 	
 
+var nroran=Math.floor(Math.random()*11);
+
+	
+
+if(nroran>=9)
+{
+	alert(nroran+" EXCELENTE");
+}
+
+else if(4<=nroran && nroran<=8)
+{
+	alert(nroran+" APROBÓ");
+}
+
+else if(nroran<4)
+{
+	alert(nroran+" Vamos, la proxima se puede");
+}
+
+
+
+
+
+
 }//FIN DE LA FUNCIÓN
