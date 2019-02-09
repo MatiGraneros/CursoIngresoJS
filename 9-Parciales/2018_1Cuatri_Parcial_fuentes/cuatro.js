49f70ab3de2ba8655ae28373e2f3ddcbe3fc9284
unknown <maati0133@gmail.com>
2019-02-09 12:37:24 -0300

    ej 4 parciales

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
 
+
+var valor1=prompt("ingrese valor");
+var valor2=prompt("ingrese el segundo valor");
+
+if(valor1==valor2){
+	alert(valor1+valor2);
+}
+else{
+	var valorEntero1=parseFloat(valor1);
+	var valorEntero2=parseFloat(valor2);
+	alert(valorEntero1+valorEntero2);
+}
+
+
+
+
+
+
 }
