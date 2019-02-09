0ebb9f47498774c69d2eea7c2d18e19cf5992e89
unknown <maati0133@gmail.com>
2019-02-09 09:49:32 -0300

    mas switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,7 +5,23 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño){
+		case "Febrero":
+		alert("Este mes no tiene más de 29 días.")
+		break;
+	 }
 
 
-}//FIN DE LA FUNCIÓN
+		if (mesDelAño!=="Febrero"){
+			alert("Este mes tiene 30 o más días");
+		}
+
+
+
+
+}
+
+
+
+
+		//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,58 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
+
+// if(mesDelAño==)
 	
+switch(mesDelAño){
+		case "Enero":
+		alert("Tiene 31 días")
+		break;
+		
+		case "Febrero":
+		alert("Tiene 28 días")
+		break;
+
+		case "Marzo":
+		alert("Tiene 31 días")
+		break;
+
+		case "Abril":
+		alert("Tiene 30 dias")
+		break;
+
+		case "Mayo":
+		alert("Tiene 31 días")
+		break;
+
+		case "Junio":
+		alert("Tiene 30 dias")
+		break;
+
+		case "Julio":
+		alert("Tiene 31 días")
+		break;
+
+		case "Agosto":
+		alert("Tiene 31 días")
+		break;
+
+		case "Septiembre":
+		alert("Tiene 30 días")
+		break;
+
+		case "Octubre":
+		alert("Tiene 31 días")
+		break;
+
+		case "Noviembre":
+		alert("Tiene 30 días")
+		break;
+
+		case "Diciembre":
+		alert("Tiene 31 días")
+		break;
+}
 
 
 
