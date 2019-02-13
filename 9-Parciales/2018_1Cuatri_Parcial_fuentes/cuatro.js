c4d9d7593e244cc94f881e22337c8aa134f28689
unknown <maati0133@gmail.com>
2019-02-13 17:08:37 -0300

    if parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -5,44 +5,53 @@ var cantidad;
 var descuento;
 var precio=500;
 var descuento2;
-var	descuento3;
-var descuento4;
 var metodoDePago;
-var TARJETA;
-var EFECTIVO;
 var recargo;
-	
+var final;	
+
+
+
 	metodoDePago=prompt("escriba el metodo de pago, si es tarjeta escribir SI, si es efectivo escribir NO");
-
-
 	cantidad=prompt("Cada producto vale $500, ingrese la Cantidad de productos");
 		parseInt(cantidad);
 
 		
 
 
- if(cantidad>=2 && metodoDePago=="NO")
-  precio=cantidad*precio;
-	precio=parseFloat(precio);
-	 	descuento=precio*10/100;
-	 		descuento=precio-descuento;
-	 {
-		 	if(precio>=2000)
-			{
-			descuento=precio*10/100;
-				precio=precio-descuento;
+ if(cantidad>=2)
+ 	{
+ 	 if(metodoDePago=="NO")
+ 		{
+ 	 	precio=cantidad*precio;
+ 	 	precio=parseFloat(precio);
+ 	 	descuento=precio*10/100;
+ 	 	final=precio-descuento;
+ 	 	if(precio>=2000)
+ 	 		{
+ 	 			descuento2=precio*15/100;
+				final=precio-descuento-descuento2;
 
-			descuento2=precio*15/100;
-				precio=precio-descuento;
 			}	 		
-	 }
+		}
 
-alert(descuento);
+	}
+				else
+				{
+					final=cantidad*precio
+				}
+
+
+
 
  
-// -10
-// -15
-// tarjeta+10
+if(metodoDePago=="SI")
+{
+	precio=cantidad*precio;
+ 	 	precio=parseFloat(precio);
+ 	 	recargo=precio*10/100;
+ 	 	final=precio+recargo;
+}
 
+alert(final);
 
 }
