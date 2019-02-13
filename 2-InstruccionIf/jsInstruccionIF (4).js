deb818f2b6621a50786d5c1d351928d6b713aa30
unknown <maati0133@gmail.com>
2019-02-12 22:10:44 -0300

    mas IF (ej 4)

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -6,8 +6,8 @@ var edad1;
 	edad1=document.getElementById('edad').value;
 	edad1=parseInt(edad1);
 
-		if(17>=edad1)
-			{
+		if (edad1<=17) 
+			{			
 			if(edad1>=13)
 				{
 				 alert("usted es adolescente");
@@ -15,12 +15,15 @@ var edad1;
 			}
 
 
+/*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/
 
-
+				if(17>=edad1 && edad1>=13)
+				{
+					alert("usted es adolescente");
+				}
 
 
 
 
 }
-
 /*FIN DE LA FUNCIÓN*/
