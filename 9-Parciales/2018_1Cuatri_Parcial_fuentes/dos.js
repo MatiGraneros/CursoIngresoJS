ccaedb780fd23a7cdbae7ad68a2cc996e24e4503
alumno <alumno>
2019-02-13 09:26:18 -0300

    ejercicio en clase 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -2,14 +2,21 @@ function mostrar()
 {
   
 
-var nombre;
-var lugar;
+var precioEfectivo;
+var porcentaje;
+var resultado;
+	
+	precioEfectivo=document.getElementById('elNombre').value;
+		precioEfectivo=parseInt(precioEfectivo);
+	
+		porcentaje=precioEfectivo*10/100;
+			porcentaje=parseInt(porcentaje);
+	
+			resultado=precioEfectivo-porcentaje
+	  			resultado=parseInt(resultado);
 
-	nombre=document.getElementById('elNombre').value;
-	lugar=document.getElementById('laLocalidad').value; 
 
-		alert("usted es "+nombre+" y vive en "+lugar);
-
+				 	alert("el precio en efectivo es: $"+precioEfectivo+", con tarjeta tiene un recargo del 10% que seria $"+porcentaje+" de recargo, cotandole final $"+resultado);
 
 
 
