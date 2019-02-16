0e5f7ce05e52cd00fa8e1dd80fbf257387ff8f28
alumno <alumno@utnfra.com.ar>
2019-02-16 12:54:52 -0300

    mejorado ej 10 if

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -10,21 +10,34 @@ var numeroRandom=Math.floor(Math.random()*10)+1;
 if(numeroRandom>=9)
 {
 	console.log(numeroRandom+" EXCELENTE");
+}	
+
+else{
+	if (numeroRandom<4)
+	 {
+	 	console.log(numeroRandom+" Vamos, la proxima se puede");
+	 }
+	 else
+	 {
+	 	console.log(numeroRandom+" APROBÓ");
+	 }
 }
 
-else 
-	{
-		if(4<=numeroRandom && numeroRandom<=8)
-		{
-			console.log(numeroRandom+" APROBÓ");
-		}
-	}
 
 
-if(numeroRandom<4)
-{
-	console.log(numeroRandom+" Vamos, la proxima se puede");
-}
+// else 
+// 	{
+// 		if(4<=numeroRandom && numeroRandom<=8)
+// 		{
+// 			console.log(numeroRandom+" APROBÓ");
+// 		}
+// 	}
+
+
+// if(numeroRandom<4)
+// {
+// 	console.log(numeroRandom+" Vamos, la proxima se puede");
+// }
 
 
 
