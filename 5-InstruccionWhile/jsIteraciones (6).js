bc7d782ef33c244da90a71f20efe66c0b3242786
alumno <alumno@utnfra.com.ar>
2019-02-16 11:45:55 -0300

    resolver while 6

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,19 +3,20 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var numeroUno=prompt("primer numero");
-	var numeroDos=prompt("segundo numero");
-	var numeroTres=prompt("tercer numero");
-	var numeroCuatro=prompt("cuarto numero");
-	var numeroCinco=prompt("quinto numero");
+	
+// 	var numeroUno=prompt("primer numero");
+// 	var numeroDos=prompt("segundo numero");
+// 	var numeroTres=prompt("tercer numero");
+// 	var numeroCuatro=prompt("cuarto numero");
+// 	var numeroCinco=prompt("quinto numero");
 
 
-while(numeroUno<5)
-{
-	numeroUno=parseInt(numeroUno)
-	numeroUno=numeroUno+1
-	alert(numeroUno);
-}
+// while(numeroUno<5)
+// {
+// 	numeroUno=parseInt(numeroUno)
+// 	numeroUno=numeroUno+1
+// 	alert(numeroUno);
+// }
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
