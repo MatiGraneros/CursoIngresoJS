0c41e33ca5989b59f76ac1632ec9efbc530bbee0
alumno <maati0133@gmail.com>
2019-02-23 11:23:12 -0300

    while

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,63 +1,70 @@
 function mostrar()
 {
 
+// 	var contador=0;
+// 	var acumulador=0;
 
+
+
+// document.getElementById('suma').value=acumulador;
+// document.getElementById('promedio').value=acumulador/5;
 	
 	
 	var numeroUno=prompt("primer numero");
-	numeroUno=parseInt(numeroUno);
+	numeroUno=parseFloat(numeroUno);
 
 	var numeroDos=prompt("segundo numero");
-	numeroDos=parseInt(numeroDos);
+	numeroDos=parseFloat(numeroDos);
 
 	var numeroTres=prompt("tercer numero");
-	numeroTres=parseInt(numeroTres);
+	numeroTres=parseFloat(numeroTres);
 
 	var numeroCuatro=prompt("cuarto numero");
-	numeroCuatro=parseInt(numeroCuatro);
+	numeroCuatro=parseFloat(numeroCuatro);
 
 	var numeroCinco=prompt("quinto numero");
- 	numeroCinco=parseInt(numeroCinco);
+ 	numeroCinco=parseFloat(numeroCinco);
 
  	var acumulador;
 		
+		while(!(numeroUno>0 || numeroUno<0))
+		{
+			console.log(numeroUno);
+				numeroUno=prompt("incorrecto el primer valor, ingrese un NÚMERO");
+				numeroUno=parseFloat(numeroUno);
+		}
+			
+		while(!(numeroDos>0 || numeroDos<0))
+		{
+			console.log(numeroDos);
+				numeroDos=prompt("incorrecto el segundo valor, ingrese un NÚMERO");
+				numeroDos=parseFloat(numeroDos);
+		}
 
-	
-	
+		while(!(numeroTres>0 || numeroTres<0))
+		{
+			console.log(numeroTres);
+				numeroTres=prompt("incorrecto el tercer valor, ingrese un NÚMERO");
+				numeroTres=parseFloat(numeroTres);
+		}
 
-while(!(numeroUno>0 || numeroUno<0))
-{
-	console.log(numeroUno);
-		numeroUno=prompt("incorrecto el primer valor, ingrese un NÚMERO");
-}
-	
-while(!(numeroDos>0 || numeroDos<0))
-{
-	console.log(numeroDos);
-		numeroDos=prompt("incorrecto el segundo valor, ingrese un NÚMERO");
-}
+		while(!(numeroCuatro>0 || numeroCuatro<0))
+		{
+			console.log(numeroCuatro);
+				numeroCuatro=prompt("incorrecto el cuarto valor, ingrese un NÚMERO");
+				numeroCuatro=parseFloat(numeroCuatro);
+		}
 
-while(!(numeroTres>0 || numeroTres<0))
-{
-	console.log(numeroTres);
-		numeroTres=prompt("incorrecto el tercer valor, ingrese un NÚMERO");
-}
+		while(!(numeroCinco>0 || numeroCinco<0))
+		{
+			console.log(numeroCinco);
+				numeroCinco=prompt("incorrecto el quinto valor, ingrese un NÚMERO");
+				numeroCinco=parseFloat(numeroCinco);
+		}
 
-while(!(numeroCuatro>0 || numeroCuatro<0))
-{
-	console.log(numeroCuatro);
-		numeroCuatro=prompt("incorrecto el cuarto valor, ingrese un NÚMERO");
-}
-
-while(!(numeroCinco>0 || numeroCinco<0))
-{
-	console.log(numeroCinco);
-		numeroCinco=prompt("incorrecto el quinto valor, ingrese un NÚMERO");
-}
-
-		
-	acumulador=numeroUno+numeroDos+numeroTres+numeroCuatro+numeroCinco;
-		document.getElementById('suma').value=acumulador;
-			document.getElementById('promedio').value=acumulador/5;
+				
+			acumulador=numeroUno+numeroDos+numeroTres+numeroCuatro+numeroCinco;
+				document.getElementById('suma').value=acumulador;
+					document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,37 @@
 function mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
-	var respuesta='si';
+	// var contador=0;
+	// var acumulador=0;
+	// var respuesta='si';
+	var numero
+
+numero=prompt("ingrese valor, parar con si");
+	if(numero<0 || numero>0)
+	{
+		numero=parseFloat(numero);		
+	}//primer ingreso
+
+while(numero!="si")
+{
+	numero=prompt("ingrese valor, parar con si");
+	if(numero<0 || numero>0)
+	{
+		numero=parseFloat(numero);		
+		numero=numero+numero;
+		alert(numero);
+	}
+}
+	
+alert(numero);
+
+
+
 
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
+
+
 
 }//FIN DE LA FUNCIÓN
