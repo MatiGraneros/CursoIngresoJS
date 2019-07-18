f0f0c57c98e6c8a75f81c14ac3f2cc98866f1c27
unknown <maati0133@gmail.com>
2019-07-18 19:10:40 -0300

    aaaaaaaaa whileeeeee meeeeeeeedz

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,33 +4,39 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var contadorNegativos=0;
 	
 	var respuesta=true;
 
 	var numero;
 
-while(respuesta==true)
+	while(respuesta==true)
 	{	
 	contador++;
 	numero=prompt("insertar numero "+ contador);
 	numero=parseFloat(numero);
 	while(isNaN(numero))
- 		{
- 		numero=prompt("insertar numero "+ contador);
+	{
+		numero=prompt("ERROR, insertar numero "+ contador);
 		numero=parseFloat(numero);
- 		}
+	}
 	if (numero>0) 
-	 	{
-	 		positivo=numero+positivo;
-	 	}
+		{
+			positivo=numero+positivo;
+		}
 		else
 		{
 			if (numero<0)
 			{
 				negativo=numero*negativo;
+				contadorNegativos++;
 			}
 		}
-	respuesta=confirm("queres otro numero? (escribi si, de lo contrario dejalo en blanco)");
+		respuesta=confirm("queres otro numero? (escribi si, de lo contrario dejalo en blanco)");
+	}
+	if(contadorNegativos<1)
+	{
+		negativo=0;
 	}
 
 document.getElementById('suma').value=positivo;
